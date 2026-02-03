46988dd524ac06cd9177ba55043d0feb3efc9fc9
Meher Raddadi <m.raddadi@yzydigital.com>
2026-02-03 16:57:55 +0100

    feat: add committia + copilot-chat keymaps and config

--- lua/plugins/copilot-chat.lua
+++ lua/plugins/copilot-chat.lua
@@ -7,7 +7,22 @@ return {
     },
     build = "make tiktoken",
     opts = {
-      -- See Configuration section for options
+      model = "gpt-4",
+      auto_insert_mode = true,
+      show_help = true,
+      question_header = "## User ",
+      answer_header = "## Copilot ",
     },
+    config = function(_, opts)
+      local chat = require("CopilotChat")
+      chat.setup(opts)
+      
+      -- Keymaps pratiques
+      vim.keymap.set('n', '<leader>cc', ':CopilotChat<CR>', { desc = 'Copilot Chat' })
+      vim.keymap.set('n', '<leader>cm', ':CopilotChatCommit<CR>', { desc = 'Generate commit message' })
+
+      vim.keymap.set('v', '<leader>ce', ':CopilotChatExplain<CR>', { desc = 'Explain code' })
+      vim.keymap.set('v', '<leader>cf', ':CopilotChatFix<CR>', { desc = 'Fix code' })
+    end,
   },
 }
